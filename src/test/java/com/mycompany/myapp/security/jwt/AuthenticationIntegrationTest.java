81ac9a19a1b469b520b0e0dfb0167fa2e39f8e5a
kientm <kientm@smartosc.com>
2024-11-01 14:09:09 +0700

    remove key

--- src/test/java/com/mycompany/myapp/security/jwt/AuthenticationIntegrationTest.java
+++ src/test/java/com/mycompany/myapp/security/jwt/AuthenticationIntegrationTest.java
@@ -16,7 +16,7 @@ import tech.jhipster.config.JHipsterProperties;
 @Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest(
     properties = {
-        "jhipster.security.authentication.jwt.base64-secret=fd54a45s65fds737b9aafcb3412e07ed99b267f33413274720ddbb7f6c5e64e9f14075f2d7ed041592f0b7657baf8",
+        "jhipster.security.authentication.jwt.base64-secret=${BASE_64_KEY}",
         "jhipster.security.authentication.jwt.token-validity-in-seconds=60000",
     },
     classes = {
